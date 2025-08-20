d4b9389edcf99a6e6a25ce5f55d98c572eddfe20
Pedro Andreotti <160793937+Pedro-Andreotti@users.noreply.github.com>
2025-08-20 01:40:39 +0000

    Configuração do lombok e criação da classe Aluno com gets, sets via lombok para variaveis private e uso da classe aluno na primeira classe

--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -3,5 +3,14 @@ package app;
 public class PrimeiraClasse {
     public static void main(String[] args) {
         System.out.println("Ola Gradle!!!");
+
+        Aluno a1 = new Aluno();
+        a1.setId(1);
+        a1.setNome("Pedro");
+        a1.setIdade(20);
+
+        System.out.println(a1.getId());
+        System.out.println(a1.getNome());
+        System.out.println(a1.getIdade());
     }
 }
